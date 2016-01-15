a7d55f7ad4b7cc60da25b6425b69d96cc45038d9
Nhat Khai Le <lnkhai@gmail.com>
2016-01-15 12:54:32 +1100

    working search and search all

--- js/addressbook.js
+++ js/addressbook.js
@@ -23,11 +23,17 @@ var contacts = {
 	]
 };
 
+var btn = document.getElementById("get-all");
+var searchField = document.getElementById("q");
+var searchForm = document.getElementById("search-form");
+var target = document.getElementById("output");
+var getAllButton = document.getElementById("get-all");
+var count = contacts.addressBook.length;
+var book = contacts.addressBook;
+
 var adr = {
 	getAllContacts : function() {
-		var target = document.getElementById("output");
-		var book = contacts.addressBook;
-		var count = book.length;
+
 		var i;
 		target.innerHTML = "";
 
@@ -45,12 +51,40 @@ var adr = {
 
 	removeActiveSection : function() {
 		this.parentNode.removeAttribute("class");
-	}
+	},
 
+	addHoverClass : function() {
+		searchForm.setAttribute("class", "hovering");
+	},
+
+	removeHoverClass : function() {
+		searchForm.removeAttribute("class");
+	},
+
+	search : function(event) {
+		event.preventDefault();
+		var target = document.getElementById("output");
+		var book = contacts.addressBook;
+		var count = book.length;
+		var i;
+		var searchValue = searchField.value;
+		target.innerHTML = "";
+
+		alert(searchValue + "--" + count);
+		if (count > 0 && searchValue !== "") {
+			for (i = 0; i < count; i++) {
+				var obj = contacts.addressBook[i], isItFound = obj.name.indexOf(searchValue);
+				if (isItFound !== -1) {
+					target.innerHTML += '<p>' + obj.name + ', <a href="mailto:' + obj.email + '">' + obj.email + '</a><p>';
+				}
+			}
+		}
+	}
 }
 
-var btn = document.getElementById("get-all");
-var searchField = document.getElementById("q");
 btn.addEventListener("click", adr.getAllContacts, false);
 searchField.addEventListener("focus", adr.addActiveSection, false);
 searchField.addEventListener("blur", adr.removeActiveSection, false);
+searchForm.addEventListener("mouseover", adr.addHoverClass, false);
+searchForm.addEventListener("mouseout", adr.removeHoverClass, false);
+searchForm.addEventListener("submit", adr.search, false);
